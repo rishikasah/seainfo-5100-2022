076e68f3c4fc64cc1cbb62bcc66bf3f5fe4643d6
rishikasah <98227949+rishikasah@users.noreply.github.com>
2022-02-19 16:47:42 -0800

    committing

---
+++ src/homework2/Person.java
@@ -0,0 +1,47 @@
+package homework2;
+
+public class Person {
+     String name;
+     int age;
+     int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+    public void getid (int id){
+        this.id = id;
+        private.int = id;
+
+        public Person(String name, int age, int id){
+            this.name = name;
+            this.age = age;
+            this.id = id
+
+
+        }
+
+    }
+}
